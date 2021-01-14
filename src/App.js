94b1c634be6f9c0185708a7adaaf29d6fa7cd466
Tanghe Kevin <tanghekevin1@gmail.com>
2021-01-14 12:57:12 +0100

    exo finis

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+
+  state = {
+    nombre: "1"
+  }
+
+  hello = () => {
+    console.log("Bonjour");
+  }
+
+  check = (e) => {
+    console.log(e.target)
+  }
   render() {
     return (
       <div>
-        <h1>Exercice 8</h1>
-        <button>Click Me</button>
+        <h1 onMouseOver={this.check}>Exercice 8</h1>
+        <button onClick={console.log(this.state.nombre)}>Click Me</button>
       </div>
     )
   }
